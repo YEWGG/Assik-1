7d1519c6b76a111ac1af636a737f12fa4eb277a3
daure <foraclient6089@gmail.com>
2025-03-29 10:40:47 +0500

    task 7(test)

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //Task 6
+        //Task 7
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of elements: ");
+        int n = scanner.nextInt();
         double startTime  = System.nanoTime();
-        double result = Tasks.power(2, 10);
+        Tasks.printReverse(n);
         double endTime  = System.nanoTime();
         double duration = (endTime - startTime)/1000000;
-        System.out.println("Result: "+result);
         System.out.println("Duration: "+duration+" milliseconds");
 
 
