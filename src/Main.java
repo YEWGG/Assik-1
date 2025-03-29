1724d875c951ab91bfbcd182da258200aae186c4
daure <foraclient6089@gmail.com>
2025-03-29 10:44:24 +0500

    task 8(test)

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,12 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //Task 7
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the number of elements: ");
-        int n = scanner.nextInt();
+        //Task 8
         double startTime  = System.nanoTime();
-        Tasks.printReverse(n);
+        String result = Tasks.isAllDigits("12A345");
         double endTime  = System.nanoTime();
         double duration = (endTime - startTime)/1000000;
+        System.out.println("Result: "+result);
         System.out.println("Duration: "+duration+" milliseconds");
 
 
