840635747d16408d20809ca5a2eab5cad052f9f4
daure <foraclient6089@gmail.com>
2025-03-29 10:30:32 +0500

    task 3(test)

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //Task 2
+        //Task 3
         double startTime  = System.nanoTime();
-        double result = Tasks.getAverage(4, new int[]{3,2,4,1});
+        String result = Tasks.isPrime(19);
         double endTime  = System.nanoTime();
         double duration = (endTime - startTime)/1000000;
         System.out.println("Result: "+result);
