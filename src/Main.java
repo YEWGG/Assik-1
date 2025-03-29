92d7c7b3955ae728933c1c981767f14741366f1e
daure <foraclient6089@gmail.com>
2025-03-29 10:47:43 +0500

    task 10(test)

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
-import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //Task 9
+        //Task 10
         double startTime  = System.nanoTime();
-        int result = Tasks.getBinomialCoefficient(7,3);
+        int result = Tasks.getGCD(32,48);
         double endTime  = System.nanoTime();
         double duration = (endTime - startTime)/1000000;
         System.out.println("Result: "+result);
