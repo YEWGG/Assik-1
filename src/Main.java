0feb4defa9bd585889c2fe9127d79d93ff2c7bcf
daure <foraclient6089@gmail.com>
2025-03-29 10:45:35 +0500

    task 9(test)

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //Task 8
+        //Task 9
         double startTime  = System.nanoTime();
-        String result = Tasks.isAllDigits("12A345");
+        int result = Tasks.getBinomialCoefficient(7,3);
         double endTime  = System.nanoTime();
         double duration = (endTime - startTime)/1000000;
         System.out.println("Result: "+result);
